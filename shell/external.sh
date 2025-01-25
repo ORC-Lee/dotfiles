4b5d421515e5fc19079136e8b7ce0a7690bce875
ORC-Lee <ORCLee@outlook.com>
2025-01-25 10:49:39 +0800

    Update external.sh

--- shell/external.sh
+++ shell/external.sh
@@ -14,3 +14,18 @@ VAGRANT_DISABLE_VBOXSYMLINKCREATE=1
 hostip=$(cat /etc/resolv.conf |grep -oP '(?<=nameserver\ ).*')
 alias setp='export https_proxy="http://${hostip}:7890"; export http_proxy="http://${hostip}:7890"; export all_proxy="socks5://${hostip}:7890"; export ALL_PROXY="socks5://${hostip}:7890";'
 alias unsetp='unset https_proxy; unset http_proxy; unset all_proxy; unset ALL_PROXY;'
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/orclee/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/orclee/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/orclee/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/orclee/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
