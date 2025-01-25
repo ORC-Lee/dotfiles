2a88476e5f750ad0b44362a7d7c3a92dbf208746
ORC-Lee <ORCLee@outlook.com>
2025-01-25 10:47:33 +0800

    Update functions.sh

--- shell/functions.sh
+++ shell/functions.sh
@@ -23,6 +23,15 @@ here() {
     echo "here -> $(readlink $HOME/.shell.here)"
 }
 
+# yazi
+function y() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        builtin cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
+}    
 there="$HOME/.shell.here"
 
 there() {
