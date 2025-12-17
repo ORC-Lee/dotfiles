8ee0531c8ac2e0ec1ebc17f41cb74282d0a5227b
ORC-Lee <ORCLee@outlook.com>
2025-12-17 15:45:58 +0800

    Delete nvim/lua/plugins/render-markdown.lua

--- nvim/lua/plugins/render-markdown.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
-	opts = {},
-}
