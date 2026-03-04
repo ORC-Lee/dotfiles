9b268461b33baeebc78ed52fcf9f28937ea71169
ORC-Lee <ORCLee@outlook.com>
2026-03-05 01:40:09 +0800

    Update nvim-pyenv-manager.lua

--- nvim/lua/plugins/nvim-pyenv-manager.lua
+++ nvim/lua/plugins/nvim-pyenv-manager.lua
@@ -1,9 +1,17 @@
 return {
-	"idossha/nvim-pyenv-manager",
-	dependencies = {
-		"nvim-telescope/telescope.nvim",
-		"nvim-lualine/lualine.nvim",
-	},
-	config = true,
-	ft = { "python" },
+  "idossha/nvim-pyenv-manager",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lualine/lualine.nvim",
+  },
+  ft = { "python" },
+  config = function()
+    require('pyenv_manager').setup({
+      -- Paths for conda environments
+      conda_paths = {
+        vim.fn.expand("/opt/miniconda3/envs"),
+        vim.fn.expand("~/.conda/envs/"),
+      },
+    })
+  end
 }
